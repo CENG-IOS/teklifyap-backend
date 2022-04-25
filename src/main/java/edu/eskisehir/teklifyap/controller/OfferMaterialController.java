6d8e2f26e5117e7458ab2a6b5937485bee3aa905
Khan <oguzhanercelik@eskisehir.edu.tr>
2022-04-26 02:09:57 +0300

    remove redundant endpoints & editing offer

--- src/main/java/edu/eskisehir/teklifyap/controller/OfferMaterialController.java
+++ src/main/java/edu/eskisehir/teklifyap/controller/OfferMaterialController.java
@@ -1,8 +1,14 @@
 package edu.eskisehir.teklifyap.controller;
 
+import edu.eskisehir.teklifyap.model.Material;
+import edu.eskisehir.teklifyap.model.Offer;
+import edu.eskisehir.teklifyap.model.OfferMaterial;
+import edu.eskisehir.teklifyap.model.request.AddingOfferMaterialRequest;
 import edu.eskisehir.teklifyap.model.request.MakingOfferRequest;
 import edu.eskisehir.teklifyap.model.response.SuccessMessage;
+import edu.eskisehir.teklifyap.service.MaterialService;
 import edu.eskisehir.teklifyap.service.OfferMaterialService;
+import edu.eskisehir.teklifyap.service.OfferService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,20 +22,35 @@ import javax.servlet.http.HttpServletRequest;
 public class OfferMaterialController {
 
     private final OfferMaterialService offerMaterialService;
-
-    @PostMapping
-    public ResponseEntity<?> makeOffer(HttpServletRequest request, @RequestBody MakingOfferRequest makingOfferRequest,
-                                       @RequestParam("user") int uid) throws Exception {
-
-        offerMaterialService.makeOffer(makingOfferRequest, uid);
-        return ResponseEntity.ok(new SuccessMessage("done", request.getServletPath(), ""));
-    }
+    private final OfferService offerService;
+    private final MaterialService materialService;
 
     //teklifin detaylarını görüntülemeye çalışırken kullanılacak. Tekliflerim sayfasında inceleye bastığında çalışır.
     @GetMapping("/getMaterialsByOffer")
     public ResponseEntity<?> getMaterialsByOffer(@RequestParam("offer") int oid) {
 
         return ResponseEntity.ok(offerMaterialService.getMaterialsByOffer(oid));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<SuccessMessage> deleteOneMaterialInOffer(HttpServletRequest request, @RequestParam("offerMaterial") int omid)
+            throws Exception {
+
+        offerMaterialService.delete(offerMaterialService.findById(omid));
+
+        return ResponseEntity.ok(new SuccessMessage("deleted", request.getServletPath(), ""));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addMaterialToOffer(HttpServletRequest request, @RequestBody AddingOfferMaterialRequest request1)
+            throws Exception {
+
+        Offer offer = offerService.findById(request1.getOid());
+        Material material = materialService.findById(request1.getMid());
+        OfferMaterial offerMaterial = new OfferMaterial(material, offer, request1.getUnitPrice());
+        offerMaterialService.save(offerMaterial);
+
+        return ResponseEntity.ok(new SuccessMessage("added", request.getServletPath(), ""));
     }
 
 }
