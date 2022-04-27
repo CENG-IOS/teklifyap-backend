12527a2b36f1251eff70ceb2f49eb91762c1a1b7
lKhanl <oguzhanercelik@eskisehir.edu.tr>
2022-04-27 22:41:29 +0300

    pdf export init

---
+++ src/main/java/edu/eskisehir/teklifyap/config/TemplateEngineConfiguration.java
@@ -0,0 +1,21 @@
+package edu.eskisehir.teklifyap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class TemplateEngineConfiguration {
+
+    @Bean
+    public ClassLoaderTemplateResolver emailTemplateResolver() {
+        ClassLoaderTemplateResolver pdfTemplateResolver = new ClassLoaderTemplateResolver();
+        pdfTemplateResolver.setPrefix("/pdf");
+        pdfTemplateResolver.setSuffix(".html");
+        pdfTemplateResolver.setTemplateMode("HTML5");
+        pdfTemplateResolver.setCharacterEncoding("UTF-8");
+        pdfTemplateResolver.setOrder(1);
+        return pdfTemplateResolver;
+    }
+
+}
